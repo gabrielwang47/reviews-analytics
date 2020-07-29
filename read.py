bda712008e406e9f096a7828a37f5940819e57ba
Gabriel Wang <ji3g4nfu6@gmail.com>
2020-07-29 22:33:32 +0800

    add filter

--- read.py
+++ read.py
@@ -15,3 +15,13 @@ for d in data:
 	print(sum_len)
 
 print('留言的平均長度為', sum_len/len(data))
+
+
+new = []
+for d in data: #d 是字串 / data 是100萬筆留言
+	if len(d) < 100: #如果字串長度小於100
+		new.append(d) #就把這個d字串, 裝到new這個清單
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0]) #印出清單的第1筆資料
+
+
